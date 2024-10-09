b54d67a3733bbc08347a5f593f70ea6d10ef2d7e
Mustafiz32 <mustafizmd045@gmail.com>
2024-10-08 18:04:12 -0700

    Add files via upload

---
+++ New.js
@@ -0,0 +1,26 @@
+var mytext = document.querySelector('#head');
+function addstyle(){
+    mytext.style.color = 'red';
+}
+
+
+var mytex = document.querySelector('#head');
+function addstyl(){
+    mytex.style.color = 'blue';
+}
+
+
+var myte = document.querySelector('#head');
+function addsty(){
+    myte.style.color = 'Fuchsia';
+}
+
+var myt = document.querySelector('#head');
+function addst(){
+    myt.style.color = 'Black';
+}
+
+var mytt = document.querySelector('#head');
+function addstt(){
+    mytt.style.color = 'orange';
+}
